72df875e80f0c2dd2624d1a4daac1812e5d051f7
suman <er.sumankumari80059@gmail.com>
2024-03-31 11:00:51 +0530

    implemented aboutus components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ForumBuilderComponent } from './components/forum-builder/forum-builder.
 import { ForumAnswerComponent } from './components/forum-answer/forum-answer.component';
 import { authGuard } from './guards/auth.guard';
 import { LoginComponent } from './components/login/login.component';
+import { aboutUsComponent } from './components/aboutUs/aboutUs.component';
 
 export const routes: Routes = [
     {
@@ -19,6 +20,10 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent
         
+    },
+    {
+        path: 'aboutUs',
+        component: aboutUsComponent
     },
 
     { path: '**',   redirectTo: 'login', pathMatch: 'full' }
---
+++ src/app/components/aboutUs/aboutUs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutUs',
+  templateUrl: './aboutUs.component.html',
+  styleUrls: ['./aboutUs.component.css']
+
+})
+
+export class aboutUsComponent{
+
+}
